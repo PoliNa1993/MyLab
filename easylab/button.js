80bac4e6a0fbe625cd615d3ea06a79645297658c
PoliNa1993 <ppoolliinnaa@hotmail.com>
2017-10-05 14:16:04 -0400

    trying to make button work

--- easylab/button.js
+++ easylab/button.js
@@ -1,19 +1,20 @@
-// Find all the buttons
-var all_btns = document.getElementsByTagName('button');
-
-// Attach event handlers
-for (btn in all_btns) {
-    btn.addEventListener('click', handleMyClick);
+$(function() {
+    $( "#datepicker" ).datepicker();
+});
+function chooseFile() {
+   $("#fileInput").click();
 }
-
-// Handle a click event
-function handleMyClick(event) {
-    // Find the table row where the click happened
-    var tr = event.target.parentNode.parentNode;
-    // Find all the data in the row
-    var tds = tr.getElementsByTagName('td');
-    var data = [];
-    for (td in tds) {
-        data.push(td.innerHTML);
-    }
-}
+function add(){
+   var dom=document.getElementById("my_table");
+   var col1="<tr><td><input type="text"></td>";
+   var col2="<tr><td><input type="text"></td>";
+   inputdateObject.value = YYYY-MM-DD;
+   var col3="<tr><td><input type="text"> YYYY-MM-DD </td>";
+   var col4="<tr><td><input type="text"></td>";
+   var col5="<tr><td><input type="text"></td>";
+   var col6="<tr><td><input type="text"></td>";
+   var col7="<tr><td><input type="text" id="datepicker" ></td>"
+   var col8="<input type="file" id="fileInput" name="fileInput" />"+
+"<button type="button" onclick="chooseFile();">choose file</button>";
+   dom.result.value=col1+col2+col3+col4+col5+col6+col7+col8;
+ }
